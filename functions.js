684ffc5f4f810b07a2ca1717fb534ffd5e4d3e2d
clarjohn <59753565+clarjohn@users.noreply.github.com>
2020-03-01 20:35:09 -0800

    hh

--- functions.js
+++ functions.js
@@ -1,103 +1,6 @@
 ///document.addEventListener('DOMContentLoaded', postButtons);
 document.addEventListener('DOMContentLoaded', trip);
-
-
-
-/*
-function postButtons(){
-    //submit
-        document.getElementById('postsub').addEventListener('click', function(event){
-          var request2 = new XMLHttpRequest();
-         // var payload = {input:null}
-        //set payload
-        var input = document.getElementById('Name').value;
-        var UserEmail = document.getElementById('Email').value;
-        var UserMessage = document.getElementById('Message').value;
-        var payload = "Name="+input+"&Email="+UserEmail+"&Message="+UserMessage;
-        ///
-          request2.open('post', 'http://web.engr.oregonstate.edu/~zhangluy/tools/class-content/form_tests/check_request.php', true);
-          request2.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-          request2.send(payload);
-        })
-    }
-
-    function postButtons(){
-        //submit
-            document.getElementById('postsub').addEventListener('click', function(event){
-              var request2 = new XMLHttpRequest();
-              var payload = {input:null}
-            //set payload
-            var UserName = document.getElementById('Name').value;
-            var UserEmail = document.getElementById('Email').value;
-            var UserMessage = document.getElementById('Message').value;
-    
-            ///
-            request2.open("GET", "http://web.engr.oregonstate.edu/~zhangluy/tools/class-content/form_tests/check_request.php?Name="+UserName+"&Email="+UserEmail+"&Message="+UserMessage, true);
-         
-            })
-        }
-        
-
-      
-
-        function Locations(locval){
-            document.getElementById("test").textContent = "Table";
-            if(locval == "Nor"){
-                var tripdetail = [{day:"1",location:"Oslo",desc:"Arrive"}
-                                  ,{day:"2",location:"Oslo",desc:"Go to Viking Museum"}
-                                  ,{day:"3",location:"Oslo",desc:"Go Sledding"}
-                                ]
-            }else if(locval == "New"){
-                var tripdetail = [{day:"1",location:"stjohn",desc:"Arrive"}
-                                 ,{day:"2",location:"stjohn",desc:"Drive to Cape Merry"}
-                                 ,{day:"3",location:"Cape Merry",desc:"see the sea birds"}
-            ]
-
-            }else if(locval == "Pat"){
-                var tripdetail = [{day:"1",location:"Santiago",desc:"Arrive"}
-                                 ,{day:"2",location:"Santiago",desc:"Explore the city"}
-                                  ,{day:"3",location:"Lake District",desc:"Arrive"}
-            ]
-            }
-            var table = document.getElementById("exTripody");
-            for(var t in tripdetail){
-                var row = table.insertRow();
-
-                 for(var d in t ){
-                     let th = row.insertCell();
-                     let text = document.createTextNode(t[d]);
-
-                     cell.appendChild(text);
-                 }
-            }
-        }
-
-
-
-        function trip(){
-            var tval = document.getElementById("trip").textContent;
-            document.getElementById("sbox").textContent = tval;
-            if(tval == "Norway"){
-                document.getElementById("title").style.backgroundImage = "url(./images/norwayflag.jpg)";
-                Locations("Nor");
-    
-            }else if(tval == "Newfoundland"){
-                document.getElementById("title").style.backgroundImage = "url(./images/newfoundland-flag.png)";
-                Locations("New");
-            }else if(tval == "Patagonia"){
-                Locations("Pat");
-            }
-
-
-
-
- 
-
-
-
-        }
-*/
-
+document.getElementById('buttonClick').addEventListener("click", exportTableToCSV);
 
         function trip(){
             var tval = document.getElementById("trip").textContent;
@@ -129,16 +32,45 @@ function postButtons(){
             }else if(tval == "Newfoundland"){
                 document.getElementById("title").style.backgroundImage = "url(./images/newfoundland-flag.png)";
                 tripdetail = [
+                                {day:"0",location:"Home",desc:"Leave for your trip"},
                                 {day:"1",location:"stjohn",desc:"Arrive"},
                                 {day:"2",location:"stjohn",desc:"Drive to Cape Merry"},
-                                {day:"3",location:"Cape Merry",desc:"see the sea birds"}
+                                {day:"3",location:"Cape Merry",desc:"see the sea birds"},
+                                {day:"4",location:"Bonvista",desc:"watch the storms roll in"},
+                                {day:"5",location:"Bull Run",desc:"go on a Puffin tour"},
+                                {day:"6",location:"Terra Nova",desc:"Look for Moose"},
+                                {day:"7",location:"Trinity",desc:"Visit the old town"},
+                                {day:"8",location:"Rsnt",desc:"Check out the root cellers"},
+                                {day:"9",location:"Twillingate",desc:"Iceberg tour"},
+                                {day:"10",location:"Brokkings",desc:"See the freshwater fjords"},
+                                {day:"10",location:"viking way",desc:"See where the viking settled in North America"},
+                                {day:"11",location:"St John",desc:"Head Back to St Johns"},
+                                {day:"12",location:"St John",desc:"Go out for the night on Geogre St"},
+                                {day:"13",location:"St John",desc:"Fly Home"},
+                                {day:"14",location:"Home",desc:"Plan your next vist"}
                             ]
             }else if(tval == "Patagonia"){
                 document.getElementById("title").style.backgroundImage = "url(./images/chile-flag.png)";
                 tripdetail = [
+                                {day:"0",location:"Home",desc:"Leave for your trip"},
                                 {day:"1",location:"Santiago",desc:"Arrive"},
                                 {day:"2",location:"Santiago",desc:"Explore the city"},
-                                {day:"3",location:"Lake District",desc:"Arrive"}
+                                {day:"3",location:"Lake District",desc:"Arrive"},
+                                {day:"4",location:"Lake District",desc:"Go for a Hike"},
+                                {day:"5",location:"Lake District",desc:"Go mountaining climbing"},
+                                {day:"6",location:"Lake District",desc:"Eat the food so Good"},
+                                {day:"7",location:"Lake District",desc:"fly to Punta Arenas"},
+                                {day:"8",location:"Punta Arenas",desc:"Check out the straight and view terra del fuego!"},
+                                {day:"9",location:"Punta Arenas",desc:"Head to torre del Paine"},
+                                {day:"10",location:"Torre del Paine",desc:"Hike up to the towers"},
+                                {day:"11",location:"Torre del Paine",desc:"Go find Gunacos"},
+                                {day:"12",location:"Torre del Paine",desc:"Go see the glacers"},
+                                {day:"13",location:"Torre del Paine",desc:"Take a bike tour"},
+                                {day:"14",location:"Torre del Paine",desc:"Head back to Punta Arenas"},
+                                {day:"15",location:"Punta Arenas",desc:"Head back to Punta Arenas"},
+                                {day:"16",location:"Punta Arenas",desc:"Head back to Santiago"},
+                                {day:"17",location:"Santiago",desc:"Fly Home"},
+                                {day:"18",location:"Home",desc:"Plan your next vist"}
                             ]    
             }
 
@@ -178,7 +110,40 @@ function postButtons(){
 
 // load list 
 
-
-
         }
       
+
+//functions to download table to csv
+
+
+
+
+
+
+function download(csv, filename) {
+    var csvF;
+    var downloadLink;
+    csvF = new Blob([csv], {type: "text/csv"});
+    downloadLink = document.createElement("a");
+    downloadLink.download = filename;
+    downloadLink.href = window.URL.createObjectURL(csvF);
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+}
+
+
+
+function exportTableToCSV() {
+    var filename = 'Your_Trip.csv';
+    var csv = [];
+    var rows = document.querySelectorAll("table tr");
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        csv.push(row.join(","));        
+    }
+    // Download CSV file
+    download(csv.join("\n"), filename);
+}
